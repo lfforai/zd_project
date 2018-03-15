f2c8c126e46c60d950f3758e39d34fb613d7a947
lf <18073072@qq.com>
2018-03-15 17:49:01 +0800

    2017.11.14

---
+++ svn/zdkj/04.软件开发/02.Debug/code_test/.idea/code_test.py
@@ -0,0 +1,86 @@
+IMAGE_HEIGHT = 60
+IMAGE_WIDTH = 160
+MAX_CAPTCHA = 4
+import numpy as np
+import tensorflow as tf
+
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+            'V', 'W', 'X', 'Y', 'Z']
+
+char_set = number + alphabet + ALPHABET + ['_']  # 如果验证码长度小于4, '_'用来补齐
+CHAR_SET_LEN = len(char_set)
+print(CHAR_SET_LEN)
+
+print(len(number))
+print(len(alphabet))
+print(len(ALPHABET))
+
+def text2vec(text):
+    text_len = len(text)
+    if text_len > MAX_CAPTCHA:
+        raise ValueError('验证码最长4个字符')
+
+    vector = np.zeros(MAX_CAPTCHA * CHAR_SET_LEN)
+
+    def char2pos(c):
+        if c == '_':
+            k = 62
+            return k
+        k = ord(c) - 48
+        if k > 9:
+            k = ord(c) - 55
+            if k > 35:
+                k = ord(c) - 61
+                if k > 61:
+                    raise ValueError('No Map')
+        return k
+
+    for i, c in enumerate(text):
+        #print text
+        idx = i * CHAR_SET_LEN + char2pos(c)
+        #print i,CHAR_SET_LEN,char2pos(c),idx
+        vector[idx] = 1
+    return vector
+
+#print text2vec('1aZ_')
+
+# 向量转回文本
+def vec2text(vec):
+    char_pos = vec.nonzero()[0]
+    text = []
+    for i, c in enumerate(char_pos):
+        char_at_pos = i  # c/63
+        char_idx = c % CHAR_SET_LEN
+        if char_idx < 10:
+            char_code = char_idx + ord('0')
+        elif char_idx < 36:
+            char_code = char_idx - 10 + ord('A')
+        elif char_idx < 62:
+            char_code = char_idx - 36 + ord('a')
+        elif char_idx == 62:
+            char_code = ord('_')
+        else:
+            raise ValueError('error')
+        text.append(chr(char_code))
+    return "".join(text)
+
+
+
+#向量（大小MAX_CAPTCHA*CHAR_SET_LEN）用0,1编码 每63个编码一个字符，这样顺利有，字符也有
+vec = text2vec("F5Sd")
+print(vec)
+text = vec2text(vec)
+print(text)  # F5Sd
+
+print("--------------------------")
+
+vec = text2vec("SFd5")
+print(vec)
+text = vec2text(vec)
+print(text)  # SFd5
+
+tf.nn.sigmoid_cross_entropy_with_logits
